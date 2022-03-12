b2cf188559a8bfe4451dae170cf0aac8b0862766
Farhan Emad <farhan48034@gmail.com>
2022-03-12 12:15:26 +0000

    Converted to a node project and made script.js comply with eslint

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 const rulesButton = document.querySelector('.rules');
-const body = document.querySelector('.game');
-const rulesPopup = document.querySelector('.rules-popup');
-const rulesTextDiv = document.querySelector('.text-div');
-const options = document.querySelectorAll('.component');
 
-rulesButton.addEventListener('click', function() {
-    if(rulesTextDiv.firstChild) {
-        return;
+const body = document.querySelector('.game');
+
+const rulesPopup = document.querySelector('.rules-popup');
+
+const rulesTextDiv = document.querySelector('.text-div');
+
+rulesButton.addEventListener('click', () => {
+  if (rulesTextDiv.firstChild) {
+    return;
+  }
+
+  const ruleImg = document.createElement('img');
+  const rulesText = document.createElement('p');
+  const cross = document.createElement('img');
+
+  cross.src = 'images/icon-close.svg';
+  cross.classList.add('cross');
+  ruleImg.classList.add('rule-img');
+
+  ruleImg.src = 'images/image-rules.svg';
+  rulesText.textContent = 'RULES';
+
+  rulesText.classList.add('rule-header');
+
+  rulesTextDiv.appendChild(rulesText);
+  rulesTextDiv.appendChild(cross);
+
+  rulesPopup.appendChild(ruleImg);
+  body.classList.add('hidden-body');
+
+  cross.addEventListener('click', () => {
+    while (rulesTextDiv.firstChild) {
+      rulesTextDiv.removeChild(rulesTextDiv.firstChild);
     }
 
-    const ruleImg = document.createElement('img');
-    const rulesText = document.createElement('p');
-    const cross = document.createElement('img');
-
-    cross.src = 'images/icon-close.svg';
-    cross.classList.add('cross');
-
-    ruleImg.classList.add('rule-img')
-    ruleImg.src = 'images/image-rules.svg';
-
-    rulesText.textContent = 'RULES';
-    rulesText.classList.add('rule-header')
-
-    rulesTextDiv.appendChild(rulesText);
-    rulesTextDiv.appendChild(cross);
-
-    rulesPopup.appendChild(ruleImg);
-    body.classList.add('hidden-body');
-
-    cross.addEventListener('click', function() {
-        while(rulesTextDiv.firstChild) {
-            rulesTextDiv.removeChild(rulesTextDiv.firstChild);
-        }
-        
-        rulesPopup.removeChild(rulesPopup.children[1]);
-        body.classList.remove('hidden-body');
-    })
+    rulesPopup.removeChild(rulesPopup.children[1]);
+    body.classList.remove('hidden-body');
+  });
 });
-
